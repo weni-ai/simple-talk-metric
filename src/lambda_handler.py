8146fc79f1dea616ec7b6eef961c9fb967b9ab87
Roger Alexandre Schwartzhaupt Nunes <103526740+RogerSchwartz@users.noreply.github.com>
2025-07-30 13:05:10 -0300

    Update lambda_handler.py

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -6,6 +6,8 @@ import time
 
 
 def send_message(prompt):
+    OPENAI_API_KEY=get_parameter('OPENAI_API_KEY')
+    client = OpenAI(api_key=OPENAI_API_KEY)
     response = client.chat.completions.create(
         model="gpt-4.1-nano",
         messages=[{"role": "user", "content": prompt}]
@@ -45,11 +47,7 @@ def classify_text(text):
     '''
     return send_message(prompt)
 
-def lambda_handler(event, context):
-    OPENAI_API_KEY=get_parameter('OPENAI_API_KEY')
-    
-    client = OpenAI(api_key=OPENAI_API_KEY)
-    
+def lambda_handler(event, context):    
     # Agora esperamos uma lista de mensagens
     messages = event.get("messages", [])
     
