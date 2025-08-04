92bead0f809413c75d52de7fbddf96ace654a206
Roger Alexandre Schwartzhaupt Nunes <103526740+RogerSchwartz@users.noreply.github.com>
2025-08-04 11:48:26 -0300

    Update lambda_handler.py

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -9,7 +9,7 @@ def send_message(prompt):
     OPENAI_API_KEY=get_parameter('OPENAI_API_KEY')
     client = OpenAI(api_key=OPENAI_API_KEY)
     response = client.chat.completions.create(
-        model="gpt-4.1-nano",
+        model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content.strip()
